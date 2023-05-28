3d19d13a82fa2bc0d5cb2f89a75f1af0d36bb451
Duy-Hiep <106466621+Duy-Hiep@users.noreply.github.com>
2023-05-28 10:28:18 +0700

    Up Product Add

--- src/app/pages/product-add/product-add.component.ts
+++ src/app/pages/product-add/product-add.component.ts
@@ -8,5 +8,24 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent {
-  
+  constructor(private formBuilder: FormBuilder, private productService: ProductService){}
+
+  productForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(6)]],
+    price: [0]
+  })
+
+  onHandleSubmit(){
+    if(this.productForm.invalid)return;
+
+    const product: IProduct = {
+      name: this.productForm.value.name || '',
+      price: this.productForm.value.price || 0
+    }
+
+    this,this.productService.addPRoduct(product).subscribe(data => {
+      console.log(data);
+      
+    })
+  }
 }
